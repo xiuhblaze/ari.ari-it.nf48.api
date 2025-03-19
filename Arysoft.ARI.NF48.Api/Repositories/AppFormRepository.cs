82ebfd0ce06919a6663e7e44455b3651d30e4f80
Adrian Castillo <xiuhblaze@gmail.com>
2025-03-19 17:55:09 -0600

    Avance en appformRepository y Service

---
+++ Arysoft.ARI.NF48.Api/Repositories/AppFormRepository.cs
@@ -0,0 +1,75 @@
+﻿using Arysoft.ARI.NF48.Api.Enumerations;
+using Arysoft.ARI.NF48.Api.Exceptions;
+using Arysoft.ARI.NF48.Api.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arysoft.ARI.NF48.Api.Repositories
+{
+    public class AppFormRepository : BaseRepository<AppForm>
+    {
+        // NACECODES
+
+        public async Task AddNaceCodeAsync(Guid id, Guid naceCodeID)
+        {
+            var _naceCodeRepository = _context.Set<NaceCode>();
+
+            var foundItem = await _model.FindAsync(id)
+                ?? throw new BusinessException("The application form to add a NACE code was not found");
+            var naceCodeItem = await _naceCodeRepository.FindAsync(naceCodeID)
+                ?? throw new BusinessException("The NACE code you're trying to relate to the application form was not found");
+
+            if (foundItem.NaceCodes.Contains(naceCodeItem))
+                throw new BusinessException("The application form already has the NACE code related");
+
+            foundItem.NaceCodes.Add(naceCodeItem);
+        } // AddNaceCodeAsync
+
+        public async Task DelNaceCodeAsync(Guid id, Guid naceCodeID)
+        {
+            var _naceCodeRepository = _context.Set<NaceCode>();
+
+            var foundItem = await _model.FindAsync(id)
+                ?? throw new BusinessException("The application form to add a NACE code was not found");
+            var naceCodeItem = await _naceCodeRepository.FindAsync(naceCodeID)
+                ?? throw new BusinessException("The NACE code you're trying to relate to the application form was not found");
+
+            if (!foundItem.NaceCodes.Contains(naceCodeItem))
+                throw new BusinessException("The NACE code is not related to the application form");
+
+            foundItem.NaceCodes.Remove(naceCodeItem);
+        } // DelNaceCodeAsync
+
+        // CONTACTS
+
+        public async Task AddContactAsync(Guid id, Guid contactID)
+        {
+            var _contactRepository = _context.Set<Contact>();
+
+            var foundItem = await _model.FindAsync(id)
+                ?? throw new BusinessException("The application form to add a Contact was not found");
+            var contactItem = await _contactRepository.FindAsync(contactID)
+                ?? throw new BusinessException("The Contact you're trying to relate to the application form was not found");
+
+            if (foundItem.Contacts.Contains(contactItem))
+                throw new BusinessException("The application form already has the Contact related");
+
+            foundItem.Contacts.Add(contactItem);
+        } // AddContactAsync
+
+        // GENERAL
+
+        public new async Task DeleteTmpByUserAsync(string username)
+        {
+            foreach (var item in await _model
+                .Where(m => m.UpdatedUser.ToLower() == username.ToLower()
+                    && m.Status == AppFormStatusType.Nothing
+                ).ToListAsync())
+            {
+                _model.Remove(item);
+            }
+        } // DeleteTmpByUserAsync
+    }
+}
---
+++ Arysoft.ARI.NF48.Api/Services/AppFormService.cs
@@ -0,0 +1,314 @@
+﻿using Arysoft.ARI.NF48.Api.CustomEntities;
+using Arysoft.ARI.NF48.Api.Enumerations;
+using Arysoft.ARI.NF48.Api.Exceptions;
+using Arysoft.ARI.NF48.Api.Models;
+using Arysoft.ARI.NF48.Api.QueryFilters;
+using Arysoft.ARI.NF48.Api.Repositories;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arysoft.ARI.NF48.Api.Services
+{
+    public class AppFormService
+    {
+        public readonly AppFormRepository _repository;
+
+        // CONSTRUCTOR
+
+        public AppFormService()
+        {
+            _repository = new AppFormRepository();
+        } // AppFormService
+
+        // METHODS
+
+        public PagedList<AppForm> Gets(AppFormQueryFilters filters)
+        {
+            var items = _repository.Gets();
+
+            // Filters
+
+            if (filters.OrganizationID.HasValue)
+            {
+                items = items.Where(m => m.OrganizationID == filters.OrganizationID);
+            }
+
+            if (filters.AuditCycleID.HasValue)
+            {
+                items = items.Where(m => m.AuditCycleID == filters.AuditCycleID);
+            }
+
+            if (filters.StandardID.HasValue)
+            {
+                items = items.Where(m => m.StandardID == filters.StandardID);
+            }
+
+            if (!string.IsNullOrWhiteSpace(filters.Text))
+            {
+                filters.Text = filters.Text.ToLower();
+                items = items.Where(m => 
+                    (m.ActivitiesScope != null && m.ActivitiesScope.ToLower().Contains(filters.Text))
+                    || (m.ProcessServiceDescription != null && m.ProcessServiceDescription.ToLower().Contains(filters.Text))
+                    || (m.LegalRequirements != null && m.LegalRequirements.ToLower().Contains(filters.Text))
+                    || (m.CriticalComplaintComments != null && m.CriticalComplaintComments.ToLower().Contains(filters.Text))
+                    || (m.AutomationLevel != null && m.AutomationLevel.ToLower().Contains(filters.Text))
+                    || (m.DesignResponsibilityJustify != null && m.DesignResponsibilityJustify.ToLower().Contains(filters.Text))
+                    || (m.CurrentCertificationsExpiration != null && m.CurrentCertificationsExpiration.ToLower().Contains(filters.Text))
+                    || (m.CurrentStandards != null && m.CurrentStandards.ToLower().Contains(filters.Text))
+                    || (m.CurrentCertificationsBy != null && m.CurrentCertificationsBy.ToLower().Contains(filters.Text) )
+                    || (m.OutsourcedProcess != null && m.OutsourcedProcess.ToLower().Contains(filters.Text))
+                    || (m.AnyConsultancyBy != null && m.AnyConsultancyBy.ToLower().Contains(filters.Text))
+                    || (m.SalesComments != null && m.SalesComments.ToLower().Contains(filters.Text))
+                    || (m.ReviewJustification != null && m.ReviewJustification.ToLower().Contains(filters.Text))
+                    || (m.ReviewComments != null && m.ReviewComments.ToLower().Contains(filters.Text))
+                    || (m.Organization != null && m.Organization.Name.ToLower().Contains(filters.Text))
+                    || (m.Standard != null && m.Standard.Name.ToLower().Contains(filters.Text))
+                    || (m.UserSales != null 
+                        && (m.UserSales.Username.ToLower().Contains(filters.Text)
+                            || m.UserSales.FirstName.ToLower().Contains(filters.Text)
+                            || m.UserSales.LastName.ToLower().Contains(filters.Text)
+                       ))
+                    || (m.UserReviewer != null
+                        && (m.UserReviewer.Username.ToLower().Contains(filters.Text)
+                            || m.UserReviewer.FirstName.ToLower().Contains(filters.Text)
+                            || m.UserReviewer.LastName.ToLower().Contains(filters.Text)
+                       ))
+                );
+            } // Text
+
+            if (filters.Status.HasValue && filters.Status != AppFormStatusType.Nothing)
+            {
+                items = items.Where(m => m.Status == filters.Status);
+            }
+            else
+            {
+                if (filters.IncludeDeleted == null) filters.IncludeDeleted = false;
+                items = (bool)filters.IncludeDeleted
+                    ? items.Where(i => i.Status != AppFormStatusType.Nothing)
+                    : items.Where(i => i.Status != AppFormStatusType.Nothing
+                        && i.Status != AppFormStatusType.Deleted);
+            }
+
+            // Order
+
+            if (filters.Order.HasValue)
+            {
+                switch (filters.Order)
+                {
+                    case AppFormOrderType.Created:
+                        items = items.OrderByDescending(m => m.Created);
+                        break;
+                    case AppFormOrderType.Organization:
+                        items = items.OrderBy(m => m.Organization.Name);
+                        break;
+                    case AppFormOrderType.CreatedDesc:
+                        items = items.OrderByDescending(m => m.Created);
+                        break;
+                    case AppFormOrderType.OrganizationDesc:
+                        items = items.OrderByDescending(m => m.Organization.Name);
+                        break;
+                    default:
+                        items = items.OrderByDescending(m => m.Created);
+                        break;
+                }
+            }
+
+            // Pagination
+
+            var pagedItems = PagedList<AppForm>
+                .Create(items, filters.PageNumber, filters.PageSize);
+
+            return pagedItems;
+        } // Gets
+
+        /// <summary>
+        /// Get a single AppForm by ID
+        /// </summary>
+        /// <param name="id">Unique id</param>
+        /// <returns></returns>
+        public async Task<AppForm> GetAsync(Guid id)
+        {
+            return await _repository.GetAsync(id);
+        } // GetAsync
+
+        public async Task<AppForm> AddAsync(AppForm item)
+        {
+            // Validate
+
+            if (string.IsNullOrEmpty(item.UpdatedUser))
+                throw new BusinessException("UpdatedUser is required");
+
+            // Set values
+
+            item.ID = Guid.NewGuid();
+            item.Status = AppFormStatusType.Nothing;
+            item.Created = DateTime.UtcNow;
+            item.Updated = DateTime.UtcNow;
+
+            // Execute queries
+
+            try
+            {
+                await _repository.DeleteTmpByUserAsync(item.UpdatedUser);
+                _repository.Add(item);
+                await _repository.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new BusinessException($"AppFormService.AddAsync: {ex.Message}");
+            }
+
+            return item;
+        } // AddAsync
+
+        public async Task<AppForm> UpdateAsync(AppForm item)
+        {
+            var foundItem = await _repository.GetAsync(item.ID)
+                ?? throw new BusinessException("The record to update was not found");
+
+            // Validate
+
+            if (string.IsNullOrEmpty(item.UpdatedUser))
+                throw new BusinessException("UpdatedUser is required");
+
+            // - No permitir cambiar la organización hasta el status (cual)??
+            // - No permitir cambiar el audit cycle hasta el status ...
+
+            // - Validaciones por status
+            if (item.Status != foundItem.Status) { // El status cambió
+                switch (item.Status)
+                {
+                    case AppFormStatusType.Nothing:
+                        item.Status = AppFormStatusType.New;
+                        break;
+                }
+            }
+
+            // Set values
+            
+            foundItem.OrganizationID = item.OrganizationID;
+            foundItem.AuditCycleID = item.AuditCycleID;
+            foundItem.StandardID = item.StandardID;
+            foundItem.UserSalesID = item.UserSalesID;
+            foundItem.UserReviewerID = item.UserReviewerID;
+
+            // ISO 9K
+            foundItem.ActivitiesScope = item.ActivitiesScope;
+            foundItem.ProcessServiceCount = item.ProcessServiceCount;
+            foundItem.ProcessServiceDescription = item.ProcessServiceDescription;
+            foundItem.LegalRequirements = item.LegalRequirements;
+            foundItem.AnyCriticalComplaint = item.AnyCriticalComplaint;
+            foundItem.CriticalComplaintComments = item.CriticalComplaintComments;
+            foundItem.AutomationLevel = item.AutomationLevel;
+            foundItem.IsDesignResponsibility = item.IsDesignResponsibility;
+            foundItem.DesignResponsibilityJustify = item.DesignResponsibilityJustify;
+
+            // General
+            foundItem.AuditLanguage = item.AuditLanguage;
+            foundItem.CurrentCertificationsExpiration = item.CurrentCertificationsExpiration;
+            foundItem.CurrentStandards = item.CurrentStandards;
+            foundItem.CurrentCertificationsBy = item.CurrentCertificationsBy;
+            foundItem.OutsourcedProcess = item.OutsourcedProcess;
+            foundItem.AnyConsultancy = item.AnyConsultancy;
+            foundItem.AnyConsultancyBy = item.AnyConsultancyBy;
+            foundItem.SalesDate = item.SalesDate;   // TODO: Este va a cambiar de lugar cuando las validaciones
+            foundItem.SalesComments = item.SalesComments;
+            foundItem.ReviewDate = item.ReviewDate; // TODO: Este va a cambiar en cuanto se apliquen las validaciones
+            foundItem.ReviewJustification = item.ReviewJustification;
+            foundItem.ReviewComments = item.ReviewComments;
+
+            foundItem.Status = item.Status;         // TODO: Validar los cambios de status
+            foundItem.Updated = DateTime.UtcNow;
+            foundItem.UpdatedUser = item.UpdatedUser;
+
+            // Execute queries
+            try
+            {   
+                _repository.Update(item);
+                await _repository.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new BusinessException($"AppFormService.UpdateAsync: {ex.Message}");
+            }
+
+            return foundItem;
+        } // UpdateAsync
+
+        public async Task DeleteAsync(AppForm item)
+        {
+            var foundItem = await _repository.GetAsync(item.ID)
+                ?? throw new BusinessException("The record to update was not found");
+
+            // Validations
+
+            if (foundItem.Status == AppFormStatusType.Deleted)
+            {
+                // TODO: Validar cuando es una eliminación física
+
+                _repository.Delete(foundItem);
+            }
+            else
+            { 
+                foundItem.Status = foundItem.Status < AppFormStatusType.Cancel
+                    ? AppFormStatusType.Cancel 
+                    : AppFormStatusType.Deleted;
+                foundItem.Updated = DateTime.UtcNow;
+                foundItem.UpdatedUser = item.UpdatedUser;
+
+                _repository.Update(foundItem);
+            }
+
+            try
+            {
+                await _repository.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new BusinessException($"AppFormService.DeleteAsync: {ex.Message}");
+            }
+        } // DeleteAsync
+
+        // NACECODES
+
+        public async Task AddNaceCodeAsync(Guid id, Guid naceCodeID)
+        { 
+            await _repository.AddNaceCodeAsync(id, naceCodeID);
+
+            // HACK: Posiblemente aquí obtener los dos objetos y validar
+            // - Que no se agregue un nace Inactivo
+            // - No agregar naces a appsforms Canceladas, Eliminadas y ver que otros casos,
+            //   tal vez de ciclos ya cerrados
+
+            try
+            {
+                await _repository.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new BusinessException($"AppFormService.AddNaceCodeAsync: {ex.Message}");
+            }
+        } // AddNaceCodeAsync
+
+        public async Task DelNaceCodeAsync(Guid id, Guid naceCodeID)
+        {
+            await _repository.DelNaceCodeAsync(id, naceCodeID);
+
+            // Igual, considerar validaciones al quitar el nacecode
+
+            try
+            {
+                await _repository.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new BusinessException($"AppFormService.DelNaceCodeAsync: {ex.Message}");
+            }
+        } // DelNaceCodeAsync
+
+        // CONTACTS
+
+
+    }
+}
